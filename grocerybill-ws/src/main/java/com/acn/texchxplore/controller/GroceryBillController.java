0195ef7ab1c043b76ef5140fab67573f11f04f55
Shareem Gelito Teofilo <sgt.project.repos@gmail.com>
2020-05-29 21:40:38 +0800

    Added repository and updated the controller

--- grocerybill-ws/src/main/java/com/acn/texchxplore/controller/GroceryBillController.java
+++ grocerybill-ws/src/main/java/com/acn/texchxplore/controller/GroceryBillController.java
@@ -1,33 +1,40 @@
 package com.acn.texchxplore.controller;
 
+import com.acn.texchxplore.impl.RegularBill;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.acn.texchxplore.domain.GroceryBill;
 import com.acn.texchxplore.entity.ShoppingClerk;
 import com.acn.texchxplore.impl.DiscountedBill;
 import com.acn.texchxplore.repository.ItemRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
+@RequestMapping("/items/bill")
 public class GroceryBillController {
+
+	@Autowired
+	private ItemRepository itemRepo;
+	private ShoppingClerk shoppingClerk;
 
 	public GroceryBillController() {
 		shoppingClerk = new ShoppingClerk("TechXplore");
 	}
 
-	@Autowired
-	private ItemRepository itemRepo;
-
-	private ShoppingClerk shoppingClerk;
-
-
-
-
+	@GetMapping("/discounted")
 	public GroceryBill getTotalDiscountedBill() {
-
 		GroceryBill grocery = new DiscountedBill(shoppingClerk);
 		grocery.setItemList(itemRepo.findAll());
-
 		return grocery;
+	}
 
+	@GetMapping("/regular")
+	public GroceryBill getTotalRegularBill() {
+		GroceryBill grocery = new RegularBill(shoppingClerk);
+		grocery.setItemList(itemRepo.findAll());
+		return grocery;
 	}
 }
